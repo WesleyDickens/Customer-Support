07ecc23d0e6b11d81bf75388e9af0af10b5a3bc8
Wesley Dickens <me@wesleydickens.com>
2024-02-21 15:44:00 -0500

    big model file load

---
+++ config.py
@@ -0,0 +1,37 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
+from sklearn.svm import SVC
+from wordcloud import WordCloud, STOPWORDS
+from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import NMF
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+
+#visualization packages
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# NLP modules we will use for text normalization
+import re #regex 
+import nltk # the natural language toolkit
+from nltk.tokenize import word_tokenize
+from nltk import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+from nltk import pos_tag
+from dotenv import load_dotenv
+from openai import OpenAI
